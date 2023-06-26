8ab994d7eab5c4cddfc4c9cc2c2e60730aa3a4c1
Siddharth Parmar <siddharthparmarone@gmail.com>
2023-06-26 11:03:36 +1000

    Brand Name Creator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the " '"+"' " sign.")
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
