2f4f797658a1d352b6b1a4dd131386c2d77f1bcb
Siddharth Parmar <siddharthparmarone@gmail.com>
2023-06-27 14:04:56 +1000

    init

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-print("Welcome to the Band Name Generator.")
-street = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("Your band name could be " + street + " " + pet)
+# If the bill was $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+# Write your code below this line 👇
+
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill?"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+
+pay = bill / people
+tipCal = bill / tip
+payableTip = tipCal / people
+totalPay = pay + payableTip
+
+print(round(totalPay, 2))
